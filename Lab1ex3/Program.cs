70ff50e2f0843debd02683aff9cce3fda0f0a110
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 16:07:18 +0200

    Add project files.

---
+++ Lab1ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+               tastaura */
+
+            Console.WriteLine("Introduceti un numar intreg: ");
+            int numar= int.Parse(Console.ReadLine());
+
+            int ultimaCifra = numar % 10;
+            Console.WriteLine("Ultima cifra a numarului intreg este: " + ultimaCifra);
+
+        }
+    }
+}
